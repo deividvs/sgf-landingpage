19b23b0048a2a79aa1e7a680524973b8b3d5fdad
Deivid Santos Oliveira <deivid.dvs@gmail.com>
2026-05-04 14:03:40 -0300

    Updated index.html

--- src/components/Subscription/SubscriptionBadge.tsx
+++
@@ -1,8 +0,0 @@
-import { useSubscription } from '../../hooks/useSubscription';
-import { Badge } from '@/components/ui/badge';
-import { CheckCircle, Clock, XCircle, Loader2 } from 'lucide-react';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
-
-export function SubscriptionBadge() {
-  return null;
-}
